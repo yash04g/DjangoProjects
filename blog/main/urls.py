7b781e0a98265582555862a680523af2aa1f634a
Yash Gupta <yash@Yashs-MacBook-Air.local>
2020-04-10 19:39:22 +0530

    second commit

--- blog/main/urls.py
+++ blog/main/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from main import views
 
 urlpatterns = [
-    path('',views.index,name = 'index')
+    path('',views.index,name = 'index'),
+    path('article/<int:pk>',views.article, name = 'get_article'),
+    path('author/<int:pk>',views.author, name = 'get_author')
 ]
 
--- blog/main/views.py
+++ blog/main/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render,get_object_or_404
+from main import models
 
 # Create your views here.
 def index(request):
-    return render(request,'main/index.html',{})
-    
+    latest_articles = models.Article.objects \
+    .all() \
+    .order_by('-createdAt')[:10]
+    context = {
+        "latest_articles" : latest_articles
+    }
+    return render(request,'main/index.html',context)
+
+def article(request,pk):
+    # try :
+    #     article = models.Article.objects.get(pk = pk) # pk => primary key
+    # except :
+    #     raise Http404()
+    article = get_object_or_404(models.Article,pk=pk)
+    context = {
+        "article" : article
+    }
+    return render(request,'main/article.html',context)
+
+def author(request,pk):
+    author = get_object_or_404(models.Author,pk=pk)
+    context = {
+        "author" : author
+    }
+    return render(request,'main/author.html',context)
